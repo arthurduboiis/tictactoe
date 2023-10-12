17dd0e8b197fa12d310cd801f7116fd422ae48f9
yazid69 <yazidlembirik69@outlook.fr>
2023-10-12 12:10:30 +0200

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to my Morpion game!</h1>
+            <p>Please select a game mode:</p>
+            <button>Player vs Player</button>
+            <button>Player vs Computer</button>
+        </div>
+    );
+};
+
+export default Home;
