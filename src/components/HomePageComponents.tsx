0bcb7ee0bbe655a2d9ff62fdd048bcb67728fcb1
Arthur DUBOIS <arthur.dbs70@gmail.com>
2024-01-11 13:58:16 +0100

    fix: comment websocket function

--- src/components/HomePageComponents.tsx
+++ src/components/HomePageComponents.tsx
@@ -4,28 +4,28 @@ import FriendRequestComponents from "./FriendRequestComponents";
 import FriendListComponents from "./friends/FriendListComponents";
 import axios from 'axios';
 
-import Echo from "laravel-echo";
+// import Echo from "laravel-echo";
 
-const options = {
-  broadcaster: 'pusher',
-  key: config.pusher.key,
-  cluster: config.pusher.cluster,
-  forceTLS: config.pusher.tls,
-  //authEndpoint is your apiUrl + /broadcasting/auth
-  authEndpoint: config.pusher.authEndpoint, 
-  // As I'm using JWT tokens, I need to manually set up the headers.
-  auth: {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      Accept: 'application/json',
-    },
-  },
-};
+// const options = {
+//   broadcaster: 'pusher',
+//   key: config.pusher.key,
+//   cluster: config.pusher.cluster,
+//   forceTLS: config.pusher.tls,
+//   //authEndpoint is your apiUrl + /broadcasting/auth
+//   authEndpoint: config.pusher.authEndpoint, 
+//   // As I'm using JWT tokens, I need to manually set up the headers.
+//   auth: {
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//       Accept: 'application/json',
+//     },
+//   },
+// };
 
-const echo = new Echo(options);
-echo.private(`App.User.${userId}`).notification((data) => {
-    console.log(data);
-});
+// const echo = new Echo(options);
+// echo.private(`App.User.${userId}`).notification((data) => {
+//     console.log(data);
+// });
 
 
 function HomePageComponents() {
@@ -72,12 +72,12 @@ function HomePageComponents() {
   } else {
     status = "Joueur: " + (xIsNext ? "X" : "O"); // xIsNext est à utiliser pour déterminer quel joueur doit jouer
   }
-  window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    encrypted: true, // set to true if you're using HTTPS
-  });
+  // window.Echo = new Echo({
+  //   broadcaster: 'pusher',
+  //   key: process.env.MIX_PUSHER_APP_KEY,
+  //   cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+  //   encrypted: true, // set to true if you're using HTTPS
+  // });
 
 
   return (
