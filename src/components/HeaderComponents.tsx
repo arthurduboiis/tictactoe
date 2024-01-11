df9e3f7d5a114d9b7bbd98b132d5489a6ac95152
Arthur DUBOIS <arthur.dbs70@gmail.com>
2024-01-11 13:54:22 +0100

    feat: friends management

--- src/components/HeaderComponents.tsx
+++ src/components/HeaderComponents.tsx
@@ -1,34 +1,47 @@
-import React from 'react';
+import React from "react";
 
 interface HeaderProps {
-    onLoginClick: () => void;
-    onRegisterClick: () => void;
+  onLoginClick: () => void;
+  onRegisterClick: () => void;
+  loggedIn: boolean;
+  logout?: () => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ onLoginClick, onRegisterClick }) => { // Sert à remplacez les props par les propriétés.
-    //React.FC est un type générique qui prend en paramètre les propriétés du composant (ici HeaderProps) et retourne un composant React.
-    // C'est quoi un type générique ? C'est un type qui peut prendre en paramètre un autre type. Ici, React.FC prend en paramètre HeaderProps.  VOIR DOC TYPESCRIPT
-    return (
-        <div className="flex justify-between items-center p-4 bg-gray-800 text-white">
-            <div>
-                <h1 className="text-2xl font-bold">Jeux</h1>
-                <ul className="flex space-x-4">
-                    <li>Jouer en solo</li>
-                    <li>Jouer en ligne</li>
-                    <li>Jouer contre l'IA</li>
-                </ul>
-            </div>
-            <div>
-                <button className="text-white" onClick={onLoginClick}>
-                    Connexion
-                </button>
-                <span className="mx-2">|</span>
-                <button className="text-white" onClick={onRegisterClick}>
-                    Pas de compte ? S'inscrire maintenant
-                </button>
-            </div>
+const Header: React.FC<HeaderProps> = ({
+  onLoginClick,
+  onRegisterClick,
+  loggedIn,
+  logout
+}) => {
+ return (
+    <div className="flex justify-between items-center p-4 bg-gray-800 text-white">
+      <div>
+        <h1 className="text-2xl font-bold">Jeux</h1>
+        <ul className="flex space-x-4">
+          <li>Jouer en solo</li>
+          <li>Jouer en ligne</li>
+          <li>Jouer contre l'IA</li>
+        </ul>
+      </div>
+
+      {loggedIn && (
+        <button className="text-white" onClick={logout}>
+          Déconnexion
+        </button>
+      )}
+      {!loggedIn && (
+        <div>
+          <button className="text-white" onClick={onLoginClick}>
+            Connexion
+          </button>
+          <span className="mx-2">|</span>
+          <button className="text-white" onClick={onRegisterClick}>
+            Pas de compte ? S'inscrire maintenant
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/components/friends/FriendListComponents.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import axios from "axios";
+import { User } from "../../context/UserContext";
+import { get } from "http";
+import PendingFriendComponents from "./PendingFriendComponents";
+import FriendsComponents from "./FriendsComponents";
+
+interface FriendRequestProps {
+  onClose: () => void;
+}
+
+enum Tab {
+  FRIENDS,
+  PENDING_FRIENDS,
+}
+
+const FriendRequestComponents: React.FC<FriendRequestProps> = ({ onClose }) => {
+  const [activeTab, setActiveTab] = React.useState(Tab.FRIENDS);
+  const [pendingFriends, setPendingFriendsList] = React.useState<User[]>([]);
+  const [friends, setFriendList] = React.useState<User[]>([]);
+
+  const getFriend = async () => {
+    const result = await axios.get(
+      process.env.REACT_APP_API_URL + "api/get-friend",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+    console.log(result.data.friends)
+    setFriendList(result.data.friends);
+
+  }
+
+  const getPendingFriend = async () => {
+    const result = await axios.get(
+      process.env.REACT_APP_API_URL + "api/pending-friend",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+    console.log(result.data.pending_friend)
+    setPendingFriendsList(result.data.pending_friend);
+  }
+
+  React.useEffect(() => {
+     
+    try {
+      if (activeTab === Tab.FRIENDS) {
+        console.log("get friend");
+        getFriend();
+      }
+      else {
+        console.log("get pending friend");
+        getPendingFriend();
+      }
+    
+    } catch (error) {
+      console.log(error);
+    }
+  } , [activeTab]);
+ 
+  const handleTabChange =  (tab: Tab) => {
+   
+    setActiveTab(tab);
+  };
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-md">
+        <div className="flex justify-center mb-4">
+          <button
+            className={`mr-4 ${activeTab === Tab.FRIENDS ? "font-bold" : ""}`}
+            onClick={() => handleTabChange(Tab.FRIENDS)}
+          >
+            Listes d'amis
+          </button>
+          <button
+            className={`${
+              activeTab === Tab.PENDING_FRIENDS ? "font-bold" : ""
+            }`}
+            onClick={() => handleTabChange(Tab.PENDING_FRIENDS)}
+          >
+            Demandes d'amis
+          </button>
+        </div>
+        {/* to put the line in dark */}
+        <hr className="mb-4 border-black" />
+
+
+        {activeTab === Tab.FRIENDS && <FriendsComponents friends={friends} />}
+        {activeTab === Tab.PENDING_FRIENDS && (
+          <PendingFriendComponents pendingFriends={pendingFriends} />
+        )}
+
+        <div className="flex flex-col items-center mt-4">
+          <button onClick={onClose}>Fermer</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FriendRequestComponents;
